1ebbcc6c453af35ee94d09dc2d7f07ef5bd0b51a
Rebeca Helen <rebecabatista165@gmail.com>
2025-03-18 06:41:57 -0300

    Cria a função sortearAmigo e exibe na tela o sorteado.

--- app.js
+++ app.js
@@ -13,18 +13,35 @@ function adicionarAmigo(){
     }
 
     // Adiciona o nome à lista, atualiza a lista e limpa o campo de entrada
+
     nomes.push(nome);
     atualizarLista();
     input.value = "";
 }
 
 function atualizarLista() {
+
     let lista = document.getElementById("listaAmigos");
     lista.innerHTML = "";
 
     nomes.forEach(nome => {
+
         let item = document.createElement("li");
         item.textContent = nome;
         lista.appendChild(item);
     });
+}
+
+function sortearAmigo(){
+    if (nomes.length === 0) {
+
+        alert("A lista está vazia. Adicione nomes antes de sortear.");
+
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * nomes.length); 
+    let amigoSorteado = nomes [indiceSorteado];
+
+    document.getElementById("resultado").innerHTML = `Amigo(a) sorteado(a): <strong>${amigoSorteado}</strong>!`;
 }
