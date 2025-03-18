d6384b968d9663212955327e3e8472b448616ddc
Rebeca Helen <rebecabatista165@gmail.com>
2025-03-18 05:47:32 -0300

    Criando funções para adicionar os nomes à lista e atualizar a lista

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 //Criando uma Array
 let nomes = [];
+
+function adicionarAmigo(){
+
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    //Condição para validar a entrada
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    // Adiciona o nome à lista, atualiza a lista e limpa o campo de entrada
+    nomes.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    nomes.forEach(nome => {
+        let item = document.createElement("li");
+        item.textContent = nome;
+        lista.appendChild(item);
+    });
+}
